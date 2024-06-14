887b98ca0b1c572475ebe51fc1d8105acc7e70a4
lauxenmaju <161649077+lauxenmaju@users.noreply.github.com>
2024-06-13 22:57:49 -0300

    "firts commit"

---
+++ app.py
@@ -0,0 +1,11 @@
+#criando versão padrão de mercado
+
+from modelos.avaliacao import avaliacao
+
+from modelos.restaurante4 import Restaurante
+
+def main():
+    Restaurante.listar_restaurante()
+
+if __name__ =='__main__':
+    main()
